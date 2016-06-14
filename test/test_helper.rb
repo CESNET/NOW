b38f048dcc9a3042048b7301f2f2135ffa60ab7d
František Dvořák <valtri@civ.zcu.cz>
2016-06-14 23:54:59 +0200

    Remove duplicity from tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,17 +1,3 @@
 require './application'
 require 'minitest/autorun'
 require 'rack/test'
-
-class LunchTest < Minitest::Test
-  include Rack::Test::Methods
-
-  def app
-    Now::Application
-  end
-
-  def test_my_default
-    get '/'
-    assert_equal Now::API_VERSION, last_response.body
-  end
-
-end
